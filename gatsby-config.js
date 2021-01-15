9090602c1d704a0fb59de937c45cca89fe1e3f1b
Grindztogo <77414605+Grindztogo@users.noreply.github.com>
2021-01-14 14:48:51 -1000

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  siteMetadata: {
+    title: `Siam Garden`,
+    description: `Siam Garden Cafe has been serving the best authentic Thai cuisine in Hawaii for more than 10 years! Come try one of our famous curries or house-made noodle dishes! Authentic Thai cuisine at it's best!`,
+    author: `@siamgarden`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-transition-link`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Poppins\:light,bold`,
+          `Open Sans\:400,700`, // you can also specify font weights and styles
+        ],
+        display: "swap",
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+};
